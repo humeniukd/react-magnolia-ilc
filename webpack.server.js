67410efa704e6d4af33caae6e52ee2a9a108540f
Dmitry Humeniuk <dmitry.humeniuk@hcl.com>
2021-07-21 11:11:39 +0200

    webpack.server config

---
+++ webpack.server.js
@@ -0,0 +1,21 @@
+/* eslint-env node */
+const config = require('./webpack.config.js');
+const path = require('path');
+const ilcWebpackPluginsFactory = require('ilc-sdk').WebpackPluginsFactory;
+
+config.entry = path.resolve(__dirname, 'src/server.js');
+config.target = 'node';
+config.output.filename = 'server.js';
+config.output.libraryTarget = 'commonjs2';
+console.log('asdf', require('./publicPathTpl')(require('./PORT.json')))
+config.plugins = ilcWebpackPluginsFactory({
+    publicPathDetection: {
+        ssrPublicPath: require('./publicPathTpl')(require('./PORT.json'))
+    }
+}).server;
+config.externals = [];
+
+
+module.exports = config;
+
+
